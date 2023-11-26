26ebf006d33a6b15a9430bef12833886bbce2e67
AkJames0114 <axmedovj0114@gmail.com>
2023-11-26 23:27:32 +0900

    updated

--- app/src/main/java/com/example/noteapp/base/BaseActivity.java
+++ app/src/main/java/com/example/noteapp/base/BaseActivity.java
@@ -1,8 +1,12 @@
 package com.example.noteapp.base;
 
+import android.view.Menu;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public abstract class BaseActivity extends AppCompatActivity {
-
-
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return ;
+    }
 }
