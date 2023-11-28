fc42008c092e53347655549c660c1e291b43fcc6
AkJames0114 <axmedovj0114@gmail.com>
2023-11-28 22:10:11 +0900

    updated

---
+++ app/src/main/java/com/example/noteapp/base/BaseFragment.java
@@ -0,0 +1,8 @@
+package com.example.noteapp.base;
+
+import androidx.fragment.app.Fragment;
+import androidx.viewbinding.ViewBinding;
+
+public abstract class BaseFragment<VB extends ViewBinding> extends Fragment {
+
+}
